1eff01b5ca6de18d4b191bcaf6a51c3caba25227
Dimitrovich702 <151694954+Dimitrovich702@users.noreply.github.com>
2024-01-31 16:45:31 +0000

    Create another version force1dc.c

---
+++ another version force1dc.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <xmmintrin.h>
+#include <malloc.h>
+
+#define N 1000
+#define rm 0.1
+
+float compute_force_sse(float *positions, float x0)
+{
+  __m128 rm2 = _mm_set1_ps(rm * rm);
+  __m128 force = _mm_setzero_ps();
+  
+  for (size_t i = 0; i < N; i += 4) {
+    __m128 pos = _mm_loadu_ps(positions + i);
+    __m128 r = _mm_sub_ps(_mm_set1_ps(x0), pos);
+    __m128 r2 = _mm_mul_ps(r, r); // r^2
+    __m128 s2 = _mm_div_ps(rm2, r2); // (rm/r)^2
+    __m128 s6 = _mm_mul_ps(_mm_mul_ps(s2, s2), s2); // (rm/r)^6
+    __m128 term = _mm_sub_ps(s6, s2); // (rm/r)^6 - (rm/r)^2
+    force = _mm_add_ps(force, _mm_div_ps(_mm_mul_ps(_mm_set1_ps(12.0 * 1.0), term), r));
+  }
+  
+  float result[4];
+  _mm_storeu_ps(result, force);
+  
+  return result[0] + result[1] + result[2] + result[3];
+}
+
+
+
+
+int main(int argc, const char **argv)
+{
+  srand48(1);
+
+  float *positions;
+  positions = (float *)malloc(N * sizeof(float));
+
+  for (size_t i = 0; i < N; i++)
+    positions[i] = drand48() + 0.1;
+
+  double start, end;
+
+  float x0[] = {0., -0.1, -0.2};
+  float f0[] = {0, 0, 0};
+
+  const size_t repetitions = 1000;
+  start = get_wtime();
+
+  __m128 x0_sse = _mm_set_ps(x0[2], x0[2], x0[1], x0[0]);
+  __m128 f0_sse = _mm_setzero_ps();
+
+  for (size_t i = 0; i < repetitions; i++) {
+    f0_sse = _mm_add_ps(f0_sse, compute_force_sse(positions, x0_sse));
+  }
+
+  _mm_storeu_ps(f0, f0_sse);
+
+  end = get_wtime();
+
+  for (size_t j = 0; j < 3; ++j)
+    printf("Force acting at x_0=%lf : %lf\n", x0[j], f0[j] / repetitions);
+
+  printf("elapsed time: %lf mus\n", 1e6 * (end - start));
+  return 0;
+}
